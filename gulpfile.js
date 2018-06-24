b1317307ab4e77e5f81aec37f55f0f12fbf70c4d
Cristi <rus.cristian@gmail.com>
2018-06-25 01:56:17 +0200

    browsesync

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssImport = require('postcss-import');
+cssImport = require('postcss-import'),
+browserSync = require('browser-sync').create();
 
 gulp.task('default', function () {
    console.log("Hooray, you created a gulp task");
@@ -17,16 +18,26 @@ gulp.task('html', function () {
 gulp.task('styles', function() {
     return gulp.src('./app/assets/styles/styles.css')
         .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
-        .pipe(gulp.dest('./dist/styles'));
+        .pipe(gulp.dest('./app/dist/styles'));
 
 });
 
 gulp.task('watch', function () {
-   watch('./app/index.html', function () {
-       gulp.start('html');
-   });
 
-   watch(('./app/assets/styles/**/*.css'), function () {
+    browserSync.init({
+        notify: false,
+        server: {
+            baseDir: "app"
+        }
+    });
+
+    watch('./app/index.html', function() {
+        browserSync.reload();
+    });
+
+
+
+    watch(('./app/assets/styles/**/*.css'), function () {
        gulp.start('styles');
    });
 });
